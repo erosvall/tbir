9668fad96487e340a83ff82ff1c9d955c6b1d251
Erik Rosvall <erik.rosvall@gmail.com>
2018-03-08 15:56:12 +0100

    it now loads data

--- project.m
+++ project.m
@@ -1,9 +1,9 @@
-%% Load dataset
-
-
+%% Load datase
+file = dataset2cell(dataset('file','qa.894.raw.train.txt','ReadVarNames',false));
+questions = file(3:2:end,1);
+answers = file(4:2:end,1);
 
 %% Transform to numerical representation
-
 
 
 %% Create and train LSTM Network
