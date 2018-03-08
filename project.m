a3f5dc3cb18131cdecc8f8edc7fa4fc087402cb3
Erik Rosvall <erik.rosvall@gmail.com>
2018-03-08 16:52:34 +0100

    added a framework from mathworks to construct lstm network, but uninstalled the wrong toolbox to run it

--- project.m
+++ project.m
@@ -7,6 +7,28 @@ answers = file(3:2:end,1);
 
 
 %% Create and train LSTM Network
+inputSize = 'auto';
+outputSize = 100; % Sort of Overfitting parameter. Higher allows for more complex models, but with overfitting
+outputMode = 'last'; % otherwise 'sequence'
+numClasses = 37;
+maxEpochs = 50;
+miniBatchSize = 27;
+shuffle = 'never';
+
+options = trainingOptions('sgdm', ...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'Shuffle', shuffle);
+
+layers = [
+    sequenceInputLayer(inputSize)
+    lstmLayer(outputSize,'OutputMode',outputMode)
+    fullyConnectedLayer(numClasses)
+    softmaxLayer
+    classificationLayer];
+
+net = trainNetwork(X,Y,layers,options);
+
 
 
 %% Output query representation
