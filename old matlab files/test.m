e8172ffbb16a4d1d9825da733db259930a764928
Erik Rosvall <erik.rosvall@gmail.com>
2018-03-16 11:59:55 +0100

    shifted things around some

---
+++ old matlab files/test.m
@@ -0,0 +1,34 @@
+
+%% Load datase
+clear;
+file = dataset2cell(dataset('file','qa.894.raw.train.txt','ReadVarNames',false));
+file = file(1:10);
+questions = file(2:2:end,1);
+answers = file(3:2:end,1);
+
+%% Transform to numerical representation
+words = {};
+for i = 2:size(file,1)
+    words = [words strsplit(erase(char(file(i,1)),','))];
+end
+words = sort(words);
+voc(:,1) = unique(words)';
+voc(:,2) = cellfun(@(x) sum(ismember(words,x)),voc(:,1),'un',0);
+
+%%
+input = zeros(size(questions,1),31);
+target = zeros(size(answers,1),7);
+for i = 1:size(questions,1)
+    qwords = strsplit(char(questions(i,1)));
+    indices = cellfun(@(x) find(strcmp(voc(:,1),x)),qwords);
+    input(i,1:size(indices,2)) = indices;
+end
+for i = 1:size(answers,1)
+    qwords = strsplit(char(answers(i,1)),', ');
+    indices = cellfun(@(x) find(strcmp(voc(:,1),x)),qwords);
+    target(i,1:size(indices,2)) = indices;
+end
+%% 
+str = questions(2:3,1);
+[ind,voc] = str2ind(str,voc);
+ind2str(ind,voc);
