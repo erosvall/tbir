06042c70e0434b03e967e853852a9f7eb18d6385
SvenHermans <swf.hermans@gmail.com>
2018-03-19 01:21:56 +0100

    sequences_to_text converts categorical sequences to text

--- lstm_autoencoder2.py
+++ lstm_autoencoder2.py
@@ -1,4 +1,3 @@
-
 # Originally adapted from https://github.com/keras-team/keras/issues/1401
 # https://github.com/keras-team/keras/blob/master/examples/lstm_seq2seq.py
 # Requires Keras and Tensorflow backend
@@ -12,34 +11,29 @@ from keras.callbacks import EarlyStopping
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import plot_model,to_categorical,print_summary
+from keras.backend import argmax
+from tensorflow import InteractiveSession
 import numpy as np
 import os.path
 
-def preprocess(text,corpus):
-	token = Tokenizer(num_words=None)
-	token.fit_on_texts(corpus)
+def preprocess(text,token):
 	text = token.texts_to_sequences(text)
-
 	text = np.array(text)
 	text = pad_sequences(text)
-
-	map(np.array,text)
-
 	text = to_categorical(text)
 	(N,sequence,voc) = text.shape
-
 	return text, N, sequence, voc
 
 def load_dataset(filename,k = 0):
 	corpus = open(filename).read().lower().splitlines()
-
+	token = Tokenizer(num_words=None)
+	token.fit_on_texts(corpus)
 	## Extracting Training data and initializing some variables for the model
-	x = corpus[0:2*k-1:2] # extract every second item from the list
-	t = corpus[1:2*k-1:2]
-	x, N, sequence, voc = preprocess(x,corpus)
-	t = preprocess(t,corpus)
-
-	return x,t,N,sequence,voc
+	x = corpus[0:2*k:2] # extract every second item from the list
+	t = corpus[1:2*k:2]
+	x, N, sequence, voc = preprocess(x,token) # (sequence,voc) is different for x & t !!
+	t,_,_,_ = preprocess(t,token)
+	return x,t,N,sequence,voc,token
 
 def build_autoencoder(latent_dimension1,latent_dimension2,voc):
 	autoencoder = Sequential()
@@ -52,8 +46,16 @@ def build_autoencoder(latent_dimension1,latent_dimension2,voc):
 	autoencoder.add(Dense(voc, activation='softmax'))
 	autoencoder.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics = ['acc'])
 	return autoencoder
-
-
+	
+def sequences_to_text(token,x):
+	reverse_word_dict = dict(map(reversed,token.word_index.items()))
+	InteractiveSession()
+	from_categorical = lambda y: argmax(y,axis=-1).eval()
+	seqs_to_words = lambda y: list(map(reverse_word_dict.get,from_categorical(y))) 
+	words_to_sentence = lambda y: ' '.join(filter(None,y))
+	word_matrix = list(map(seqs_to_words,x))
+	sentence_list = list(map(words_to_sentence,word_matrix))
+	return sentence_list
 
 #Dimensionality reduction in encoder1 and encoder 2
 latent_dimension1 = 140 
@@ -64,7 +66,7 @@ file_id = 'Autoencoder_' +str(epochs)+'_'+ str(latent_dimension1) + '_' + str(la
 
 if load_data:
 	# This function fetches the dataset from the file and fills both X and T with k number of datapoints
-	train_x, train_t,N,sequence,voc = load_dataset("qa.894.raw.train.txt",100)
+	train_x, train_t,N,sequence,voc,token = load_dataset("qa.894.raw.train.txt",100)
 
 ## Build and train Autoencoder
 if os.path.exists(file_id):
