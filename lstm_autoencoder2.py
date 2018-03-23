6acffa0a289717d6032bed212129ecdf36db04fc
Erik Rosvall <erik.rosvall@gmail.com>
2018-03-23 13:19:41 +0100

    Changed to vector answer, instead of matrix

--- lstm_autoencoder2.py
+++ lstm_autoencoder2.py
@@ -5,7 +5,7 @@
 
 #from keras.layers import containers
 from keras.models import Sequential, Model,load_model
-from keras.layers import Dense, Dropout,Masking
+from keras.layers import Dense, Dropout,Masking, Flatten
 from keras.layers.recurrent import LSTM
 from keras.callbacks import EarlyStopping
 from keras.preprocessing.text import Tokenizer
@@ -15,6 +15,7 @@ from keras.backend import argmax
 from tensorflow import InteractiveSession
 import numpy as np
 import os.path
+import tensorflow as tf
 
 def preprocess(text,token):
 	text = token.texts_to_sequences(text)
@@ -46,21 +47,20 @@ def build_autoencoder(l1,l2,voc,x,e):
 
 def build_classifier(source_model,voc,x,t,e,l1,l2):
 	classifier = Sequential()
-	#Think we may need to work with a masking layer here to avoid the zeros
 	classifier.add(LSTM(l1,return_sequences=True, input_shape = (None,voc)))
-	classifier.add(LSTM(l2,return_sequences=True))
+	classifier.add(LSTM(l2,return_sequences=False))
 	classifier.layers[0].set_weights(source_model.layers[0].get_weights())
 	classifier.layers[0].trainable = False # Ensure that we don't change representation weights
 	classifier.layers[1].set_weights(source_model.layers[1].get_weights())
 	classifier.layers[1].trainable = False # Ensure that we don't change representation weights
-	classifier.add(Dense(voc,activation='softmax'))
-	classifier.compile(loss='categorical_crossentropy',optimizer='RMSprop',metrics = ['acc'])
+	classifier.add(Dense(voc,activation='softmax',name='classification_layer'))
+	classifier.compile(loss='categorical_crossentropy',optimizer='adam',metrics = ['acc'])
+	classifier.summary()
 	classifier.fit(x,t, epochs = e)
-	# We should look at fine tuning as well, basically evaluate  on train_t
 	return classifier
 
 def sequences_to_text(token,x):
-	print('Converting to text...')
+	print('Converting to text sequence...')
 	reverse_word_dict = dict(map(reversed,token.word_index.items()))
 	InteractiveSession()
 	from_categorical = lambda y: argmax(y,axis=-1).eval()
@@ -69,6 +69,14 @@ def sequences_to_text(token,x):
 	word_matrix = list(map(seqs_to_words,x))
 	sentence_list = list(map(words_to_sentence,word_matrix))
 	return sentence_list
+
+def matrix_to_text(token,x):
+	print('Converting to text vector...')
+	reverse_word_dict = dict(map(reversed,token.word_index.items()))
+	InteractiveSession()
+	seqs_to_words = lambda y: list(map(reverse_word_dict.get,argmax(y,axis=-1).eval()))
+
+	return seqs_to_words(x)
 
 #Dimensionality reduction in encoder1 and encoder 2
 
@@ -80,7 +88,7 @@ file_id = 'Autoencoder_' +str(epochs)+'_'+ str(ld1) + '_' + str(ld2) +'.h5'
 
 # This function fetches the dataset from the file and fills both X and T with k number of datapoints
 train_x, train_t,train_N,train_sequence,voc,train_token = load_dataset("qa.894.raw.train.txt",1000)
-test_x, test_t,test_N,test_sequence,_,_ = load_dataset("qa.894.raw.test.txt",4000,train_token)
+test_x, test_t,test_N,test_sequence,_,_ = load_dataset("qa.894.raw.test.txt",400,train_token)
 
 ## Build and train Autoencoder
 if os.path.exists(file_id):
@@ -94,8 +102,20 @@ print('Autoencoder parameters')
 autoencoder.summary()
 
 
-classifier = build_classifier(autoencoder,voc,train_x,train_t,10,ld1,ld2)
-print(classifier.evaluate(test_x,test_t))
+
+
+
+#answer = autoencoder.predict(train_x)
+#ind = np.random.choice(1000,10)
+#print(sequences_to_text(train_token,answer[ind]))
+#print(ind)
+new_test_t = test_t[:,-1,:]
+
+print(new_test_t.count(2))
+classifier = build_classifier(autoencoder,voc,train_x,train_t[:,-1,:],150,ld1,ld2)
+print(classifier.evaluate(test_x,new_test_t))
 answer = classifier.predict(test_x)
-print(sequences_to_text(train_token,answer[np.random.choice(4000,10)]))
+ind = np.random.choice(400,10)
+print(matrix_to_text(train_token,answer[ind]))
+print(matrix_to_text(train_token,new_test_t[ind]))
 
